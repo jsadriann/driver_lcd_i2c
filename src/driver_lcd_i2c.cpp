c39a39107e2effc5f502b0f4a818f6e0c6d578ea
jsadriann <jadriannassilva@gmail.com>
2024-11-16 12:39:10 -0300

    atualizando a lib

--- src/driver_lcd_i2c.cpp
+++ src/driver_lcd_i2c.cpp
@@ -5,7 +5,7 @@ DriverLCD_I2C::DriverLCD_I2C(uint8_t lcd_Addr, uint8_t lcd_cols, uint8_t lcd_row
   : lcd(lcd_Addr, lcd_cols, lcd_rows), _lcdAddr(lcd_Addr), _lcdCols(lcd_cols), _lcdRows(lcd_rows) {
     // Inicializa o LCD com o endereço I2C, número de colunas e linhas
     lcd.init();  // Inicializa o LCD com a função init() que você especificou
-    //lcd.clear();  // Limpa o LCD logo após inicializar
+    lcd.clear();  // Limpa o LCD logo após inicializar
     
     // Inicializa as variáveis de posição do cursor
     _cursorCol = 0;  // Coluna inicial do cursor
@@ -17,45 +17,6 @@ DriverLCD_I2C::DriverLCD_I2C(uint8_t lcd_Addr, uint8_t lcd_cols, uint8_t lcd_row
 
 // Destruidor
 DriverLCD_I2C::~DriverLCD_I2C() {
-}
-
-// Método para exibir o ângulo no LCD
-void DriverLCD_I2C::postAngle(float angle) {
-    // Cria a string "Angle: " mais o valor do ângulo
-    char angleStr[10];  // Buffer para armazenar a string convertida
-
-    // Usando sprintf para formatar o float em string com 2 casas decimais
-    sprintf(angleStr, "%.2f", angle);  // Converte o valor float para string com 2 casas decimais
-
-    String fullMessage = String("Angle: ") + angleStr;  // Junta "Angle: " com o valor do ângulo
-
-    // Verifica se a mensagem cabe na linha
-    if (fullMessage.length() > _lcdCols) {
-        // Caso não caiba, centraliza "Angle: " na primeira linha
-        ///lcd.clear();
-        int angleMessageLength = 8;  // "Angle: " tem 8 caracteres
-        int angleStrLength = strlen(angleStr);
-        
-        // Centraliza "Angle: "
-        lcd.setCursor((_lcdCols - angleMessageLength) / 2, _cursorRow);  // Calcula a posição para centralizar "Angle: "
-        lcd.print("Angle: ");
-        
-        // Centraliza o valor do ângulo na segunda linha
-        _cursorRow = 1;  // Muda para a linha 1
-        lcd.setCursor((_lcdCols - angleStrLength) / 2, _cursorRow);  // Centraliza o valor do ângulo
-        lcd.print(angleStr);  // Exibe o valor do ângulo
-    } else {
-        // Caso a mensagem caiba na linha, imprime tudo na mesma linha
-        //lcd.clear();  // Limpa a tela antes de imprimir
-        lcd.setCursor(0, 0);  // Posiciona o cursor na linha 0, coluna 0
-        lcd.print("Angle: ");
-        lcd.print(angleStr);  // Exibe o valor do ângulo
-        
-        // Atualiza o cursor para a próxima linha após imprimir
-        _cursorCol = 0;
-        _cursorRow = 1;
-        lcd.setCursor(_cursorCol, _cursorRow);  // Atualiza o cursor no LCD
-    }
 }
 
 // Getter para a coluna do cursor
@@ -72,7 +33,6 @@ uint8_t DriverLCD_I2C::getCursorRow() const {
 void DriverLCD_I2C::setCursorCol(uint8_t col) {
     if (col < _lcdCols) {  // Verifica se a coluna está dentro dos limites
         _cursorCol = col;  // Atualiza a coluna
-        lcd.setCursor(_cursorCol, _cursorRow);  // Atualiza a posição do cursor no LCD
     }
 }
 
@@ -80,6 +40,299 @@ void DriverLCD_I2C::setCursorCol(uint8_t col) {
 void DriverLCD_I2C::setCursorRow(uint8_t row) {
     if (row < _lcdRows) {  // Verifica se a linha está dentro dos limites
         _cursorRow = row;  // Atualiza a linha
-        lcd.setCursor(_cursorCol, _cursorRow);  // Atualiza a posição do cursor no LCD
     }
+}
+
+// Método para exibir o ângulo no LCD
+void DriverLCD_I2C::postAngle(float angle) {
+    // Cria a string "Angle: " mais o valor do ângulo
+    char angleStr[10];  // Buffer para armazenar a string convertida
+
+    // Usando sprintf para formatar o float em string com 2 casas decimais
+    sprintf(angleStr, "%.2f", angle);  // Converte o valor float para string com 2 casas decimais
+
+    String fullMessage = String("Angle: ") + angleStr;  // Junta "Angle: " com o valor do ângulo
+
+    // Verifica se a mensagem cabe na linha
+    if (fullMessage.length() > (_lcdCols - _cursorCol)) {
+        // Caso não caiba, centraliza "Angle: " na primeira linha
+        clear();
+        int angleMessageLength = 7;  // "Angle:" tem 7 caracteres
+        int angleStrLength = strlen(angleStr);
+        
+        // Centraliza "Angle: "
+        lcd.setCursor((_lcdCols - angleMessageLength) / 2, _cursorRow);  // Calcula a posição para centralizar "Angle: "
+        lcd.print("Angle:");
+        _cursorRow++;
+        // Centraliza o valor do ângulo na segunda linha
+        lcd.setCursor((_lcdCols - angleStrLength) / 2, _cursorRow);  // Centraliza o valor do ângulo
+        lcd.print(angleStr);  // Exibe o valor do ângulo
+	_cursorRow++;
+	_cursorCol = 0;
+    } else {
+        // Caso a mensagem caiba na linha, imprime tudo na mesma linha
+        lcd.setCursor(_cursorCol, _cursorRow);  // Posiciona o cursor
+        lcd.print("Angle: ");
+        lcd.print(angleStr);  // Exibe o valor do ângulo
+        
+        // Atualiza o cursor para a próxima linha após imprimir
+        _cursorCol = 0;
+        _cursorRow++;
+        lcd.setCursor(_cursorCol, _cursorRow);  // Atualiza o cursor no LCD
+	_cursorRow = (_cursorRow >= _lcdRows) ? 0 : _cursorRow;
+    }
+}
+
+void DriverLCD_I2C::postTofRight(float distance) {
+    // Cria a string "TOF Right: " mais o valor da distância
+    char distanceStr[10];  // Buffer para armazenar a string convertida
+
+    // Usando sprintf para formatar o float em string com 2 casas decimais
+    sprintf(distanceStr, "%.2f", distance);  // Converte o valor float para string com 2 casas decimais
+
+    String fullMessage = String("TOF Right: ") + distanceStr;  // Junta "TOF Right: " com o valor da distância
+
+    // Verifica se a mensagem cabe na linha
+    if (fullMessage.length() > (_lcdCols - _cursorCol)) {
+	clear();  // Limpa a tela antes de imprimir
+        // Caso não caiba, centraliza "TOF Right:" na primeira linha
+        int tofMessageLength = 11;  // "TOF Right:" tem 11 caracteres
+        int distanceStrLength = strlen(distanceStr);
+        
+        // Centraliza "TOF Right:"
+        lcd.setCursor((_lcdCols - tofMessageLength) / 2, 0);  // Calcula a posição para centralizar "TOF Right:"
+        lcd.print("TOF Right:");
+        _cursorRow++;
+        // Centraliza o valor da distância na segunda linha
+        lcd.setCursor((_lcdCols - distanceStrLength) / 2, 1);  // Centraliza o valor da distância
+        lcd.print(distanceStr);  // Exibe o valor da distância
+	_cursorRow++;
+        _cursorCol = 0;
+    } else {
+	// Caso a mensagem caiba na linha, imprime tudo na mesma linha
+        lcd.setCursor(_cursorCol, _cursorRow);  // Posiciona o cursor
+        lcd.print("TOF Right: ");
+        lcd.print(distanceStr);  // Exibe o valor da distância
+        // Atualiza o cursor interno
+        _cursorCol = 0;
+	_cursorRow++;
+        _cursorRow = (_cursorRow >= _lcdRows) ? 0 : _cursorRow;
+    }
+}
+
+void DriverLCD_I2C::postTofLeft(float distance) {
+    // Cria a string "TOF Left: " mais o valor da distancia
+    char distanceStr[10];  // Buffer para armazenar a string convertida
+
+    // Usando sprintf para formatar o float em string com 2 casas decimais
+    sprintf(distanceStr, "%.2f", distance);  // Converte o valor float para string com 2 casas decimais
+
+    String fullMessage = String("TOF Left: ") + distanceStr;  // Junta "TOF Left: " com o valor da distancia
+
+    // Verifica se a mensagem cabe na linha
+    if (fullMessage.length() > (_lcdCols - _cursorCol)) {
+	clear();  // Limpa a tela antes de imprimir
+        // Caso não caiba, centraliza "TOF Left:" na primeira linha
+        int tofMessageLength = 10;  // "TOF Left:" tem 10 caracteres
+        int distanceStrLength = strlen(distanceStr);
+        // Centraliza "TOF Left:"
+        lcd.setCursor((_lcdCols - tofMessageLength) / 2, _cursorRow);  // Calcula a posição para centralizar "TOF Left:"
+        lcd.print("TOF Left:");
+        _cursorRow++;
+        // Centraliza o valor da distância na segunda linha
+        lcd.setCursor((_lcdCols - distanceStrLength) / 2, _cursorRow);  // Centraliza o valor da distância
+        lcd.print(distanceStr);  // Exibe o valor da distância
+        // Atualiza o cursor interno
+        _cursorRow++;
+        _cursorCol = 0;
+    } else {
+	// Caso a mensagem caiba na linha, imprime tudo na mesma linha
+        lcd.setCursor(_cursorCol, _cursorRow);  // Posiciona o cursor
+        lcd.print("TOF Left: ");
+        lcd.print(distanceStr);  // Exibe o valor da distância
+        
+        // Atualiza o cursor interno
+        _cursorCol = 0;
+	_cursorRow++;
+        _cursorRow = (_cursorRow >= _lcdRows) ? 0 : _cursorRow;
+    }
+}
+
+void DriverLCD_I2C::postAccelerometer(float acceleration) {
+    // Cria a string "Acceleration: " mais o valor da aceleração
+    char accelerationStr[10];  // Buffer para armazenar a string convertida
+
+    // Usando sprintf para formatar o float em string com 2 casas decimais
+    sprintf(accelerationStr, "%.2f", acceleration);  // Converte o valor float para string com 2 casas decimais
+
+    String fullMessage = String("TOF Right: ") + accelerationStr;  // Junta "Acceleration: " com o valor da aceleração
+
+    // Verifica se a mensagem cabe na linha
+    if (fullMessage.length() > (_lcdCols - _cursorCol)) {
+	clear();  // Limpa a tela antes de imprimir
+        // Caso não caiba, centraliza "Acceleration:" na primeira linha
+        int tofMessageLength = 14;  // "Acceleration:" tem 14 caracteres
+        int accelerationStrLength = strlen(accelerationStr);
+        // Centraliza "Acceleration:"
+        lcd.setCursor((_lcdCols - tofMessageLength) / 2, _cursorRow);  // Calcula a posição para centralizar "Acceleration:"
+        lcd.print("Acceleration:");
+        _cursorRow++;
+        // Centraliza o valor da distância na segunda linha
+        lcd.setCursor((_lcdCols - accelerationStrLength) / 2, _cursorRow);  // Centraliza o valor da aceleraçã
+        lcd.print(accelerationStr);  // Exibe o valor da aceleração
+        // Atualiza o cursor interno
+        _cursorRow++;
+        _cursorCol = 0;
+    } else {
+	// Caso a mensagem caiba na linha, imprime tudo na mesma linha
+        lcd.setCursor(_cursorCol, _cursorRow);  // Posiciona o cursor
+        lcd.print("Acceleration: ");
+        lcd.print(accelerationStr);  // Exibe o valor da aceleração
+        
+        // Atualiza o cursor interno
+        _cursorCol = 0;
+	_cursorRow++;
+        _cursorRow = (_cursorRow >= _lcdRows) ? 0 : _cursorRow;
+    }
+}
+
+void DriverLCD_I2C::postSetPoint(float angle) {
+    // Cria a string "SetPoint: " mais o valor da angulação
+    char setPointStr[10];  // Buffer para armazenar a string convertida
+
+    // Usando sprintf para formatar o float em string com 2 casas decimais
+    sprintf(setPointStr, "%.2f", angle);  // Converte o valor float para string com 2 casas decimais
+
+    String fullMessage = String("SetPoint: ") + setPointStr;  // Junta "SetPoint: " com o valor da angulação
+
+    // Verifica se a mensagem cabe na linha
+    if (fullMessage.length() > (_lcdCols - _cursorCol)) {
+	clear();  // Limpa a tela antes de imprimir
+        // Caso não caiba, centraliza "SetPoint:" na primeira linha
+        int tofMessageLength = 10;  // "SetPoint:" tem 10 caracteres
+        int setPointStrLength = strlen(setPointStr);
+        // Centraliza "SetPoint:"
+        lcd.setCursor((_lcdCols - tofMessageLength) / 2, _cursorRow);  // Calcula a posição para centralizar "SetPoint:"
+        lcd.print("SetPoint:");
+        _cursorRow++;
+        // Centraliza o valor da angulação na segunda linha
+        lcd.setCursor((_lcdCols - setPointStrLength) / 2, _cursorRow);  // Centraliza o valor da angulação
+        lcd.print(setPointStr);  // Exibe o valor da angulação
+        // Atualiza o cursor interno
+        _cursorRow++;
+        _cursorCol = 0;
+    } else {
+	// Caso a mensagem caiba na linha, imprime tudo na mesma linha
+        lcd.setCursor(_cursorCol, _cursorRow);  // Posiciona o cursor
+        lcd.print("SetPoint: ");
+        lcd.print(setPointStr);  // Exibe o valor da angulação
+        
+        // Atualiza o cursor interno
+        _cursorCol = 0;
+	_cursorRow++;
+        _cursorRow = (_cursorRow >= _lcdRows) ? 0 : _cursorRow;
+    }
+}
+
+void DriverLCD_I2C::postOperatingMode(Modo modo) {
+    // Declara as strings para os modos
+    const char* modeLine1 = nullptr;
+    const char* modeLine2 = nullptr;
+
+    // Determina as mensagens a serem exibidas com base no modo
+    switch (modo) {
+        case Modo::Manual:
+            modeLine1 = "MANUAL";
+            modeLine2 = "MODE";
+            break;
+        case Modo::Automatico:
+            modeLine1 = "AUTOMATIC";
+            modeLine2 = "MODE";
+            break;
+        case Modo::Ensaio:
+            modeLine1 = "REHEARSAL";
+            modeLine2 = "MODE";
+            break;
+    }
+
+    // Centraliza e imprime a primeira linha
+    lcd.setCursor((_lcdCols - strlen(modeLine1)) / 2, _cursorRow);
+    lcd.print(modeLine1);
+    _cursorRow++;
+    // Centraliza e imprime a segunda linha
+    lcd.setCursor((_lcdCols - strlen(modeLine2)) / 2, _cursorRow);
+    lcd.print(modeLine2);
+
+    // Atualiza o cursor interno
+    _cursorCol = 0;
+    _cursorRow++;
+    _cursorRow = (_cursorRow >= _lcdRows) ? 0 : _cursorRow;
+}
+
+void DriverLCD_I2C::postConsumption(float voltage, float current, const char* voltageUnit, const char* currentUnit) {
+    // Imprime "Consumption:" centralizado na primeira linha
+    const char* title = "Consumption:";
+    lcd.setCursor((_lcdCols - strlen(title)) / 2, _cursorRow);  // Configura o cursor na linha 0
+    lcd.print(title);  // Imprime o título
+    _cursorRow++;
+
+    // Formata os valores de voltagem e corrente com 3 casas decimais inicialmente
+    char voltageStr[10], currentStr[10];
+    sprintf(voltageStr, "%.3f", voltage);  // Converte a voltagem para string com 3 casas decimais
+    sprintf(currentStr, "%.3f", current);  // Converte a corrente para string com 3 casas decimais
+
+    // Monta a linha de saída inicial
+    String line2 = String(voltageStr) + voltageUnit + " " + currentStr + currentUnit;
+
+    // Verifica se a linha cabe no LCD
+    if (line2.length() > _lcdCols) {
+        // Reduz para 2 casas decimais
+        sprintf(voltageStr, "%.2f", voltage);
+        sprintf(currentStr, "%.2f", current);
+        // Atualiza a linha com valores reduzidos
+        line2 = String(voltageStr) + voltageUnit + " " + currentStr + currentUnit;
+    }
+
+    if (line2.length() > _lcdCols) {
+        line2 = line2.substring(0, _lcdCols);  // Trunca caso necessario
+    }
+
+    // Centraliza a segunda linha
+    lcd.setCursor((_lcdCols - line2.length()) / 2, _cursorRow);  // Configura o cursor
+    lcd.print(line2);
+
+    // Atualiza o cursor interno
+    _cursorCol = 0;
+    _cursorRow++;
+    _cursorRow = (_cursorRow >= _lcdRows) ? 0 : _cursorRow;
+}
+
+void DriverLCD_I2C::dashboardLightOn(){
+    lcd.noBacklight();
+}
+
+void DriverLCD_I2C::dashboardLightOff(){
+    lcd.backlight();
+}
+
+void DriverLCD_I2C::clear(){
+    lcd.clear();
+    _cursorRow = 0;
+    _cursorCol = 0;
+}
+void DriverLCD_I2C::clearLine(uint8_t line) {
+    // Verifica se a linha está dentro do limite do LCD
+    if (line >= _lcdRows) return;
+
+    // Move o cursor para o início da linha desejada
+    lcd.setCursor(0, line);
+    _cursorRow = line;
+    _cursorCol = 0;
+    // Sobrescreve a linha com espaços
+    for (uint8_t i = 0; i < _lcdCols; i++) {
+        lcd.print(" ");
+    }
+
+    // atualiza o cursor
+    lcd.setCursor(_cursorCol , _cursorRow);
 }
--- src/driver_lcd_i2c.h
+++ src/driver_lcd_i2c.h
@@ -22,31 +22,165 @@ class DriverLCD_I2C {
         Automatico,
         Ensaio
     };
-    // Construtor que inicializa o LCD com o endereço I2C, número de colunas e linhas
+   /**
+    * @brief Construtor da classe DriverLCD_I2C.
+    * 
+    * Este construtor inicializa o display LCD I2C com o endereço especificado,
+    * número de colunas e linhas. Além disso, configura o cursor inicial e limpa o display
+    * após a inicialização.
+    * 
+    * @param lcd_Addr Endereço I2C do display LCD.
+    * @param lcd_cols Número de colunas do display LCD.
+    * @param lcd_rows Número de linhas do display LCD.
+    */
     DriverLCD_I2C(uint8_t lcd_Addr, uint8_t lcd_cols, uint8_t lcd_rows);
-
+   /**
+    * @brief Destruidor da classe DriverLCD_I2C.
+    * 
+    * Este destruidor é responsável por liberar quaisquer recursos alocados
+    * durante o uso da classe DriverLCD_I2C.
+    */
     // Destruidor
     ~DriverLCD_I2C();
 
-    // Métodos getter para obter a posição do cursor
+   /**
+    * @brief Obtém a coluna atual do cursor no LCD.
+    * 
+    * Este método retorna o valor da coluna onde o cursor está atualmente posicionado
+    * no display LCD.
+    * 
+    * @return uint8_t A coluna atual do cursor.
+    */
     uint8_t getCursorCol() const;
+    /**
+    * @brief Obtém a linha atual do cursor no LCD.
+    * 
+    * Este método retorna o valor da linha onde o cursor está atualmente posicionado
+    * no display LCD.
+    * 
+    * @return uint8_t A linha atual do cursor.
+    */
     uint8_t getCursorRow() const;
 
-    // Métodos setter para definir a posição do cursor
+   /**
+    * @brief Define a coluna do cursor no LCD.
+    * 
+    * Este método define a coluna onde o cursor será posicionado no LCD.
+    * O valor fornecido deve estar dentro do limite de colunas do LCD.
+    * 
+    * @param col A coluna onde o cursor será posicionado (de 0 a _lcdCols-1).
+    */
     void setCursorCol(uint8_t col);
+   /**
+    * @brief Define a linha do cursor no LCD.
+    * 
+    * Este método define a linha onde o cursor será posicionado no LCD.
+    * O valor fornecido deve estar dentro do limite de linhas do LCD.
+    * 
+    * @param row A linha onde o cursor será posicionado (de 0 a _lcdRows-1).
+    */
     void setCursorRow(uint8_t row);
-
-    //Metodos Gerais
+   /**
+    * @brief Exibe o valor do ângulo no LCD.
+    * 
+    * Este método exibe o valor do ângulo no LCD. A string "Angle: " é seguida pelo valor do ângulo formatado com 2 casas decimais.
+    * Se o valor não couber na linha, ele é centralizado na tela, dividindo em duas linhas.
+    * 
+    * @param angle O valor do ângulo a ser exibido no LCD.
+    */
     void postAngle(float angle);
-    void postTofRight(float distance);
-    void postTofLeft(float distance);
-    void postAccelerometer(float acceleration);
-    void postSetPoint(float angle);
-    void postOperatingMode(Modo modo);
-    void postConsumption(float voltage, float current);
 
-    // Método para limpar o LCD
+   /**
+    * @brief Exibe a distância medida pelo sensor TOF da direita no LCD.
+    * 
+    * Este método exibe o valor da distância do sensor TOF da direita no LCD. A string "TOF Right: " é seguida pelo valor da distância formatado com 2 casas decimais.
+    * Se o valor não couber na linha, ele é centralizado na tela, dividindo em duas linhas.
+    * 
+    * @param distance O valor da distância a ser exibido no LCD.
+    */
+    void postTofRight(float distance);
+
+   /**
+    * @brief Exibe a distância medida pelo sensor TOF da esquerda no LCD.
+    * 
+    * Este método exibe o valor da distância do sensor TOF da esquerda no LCD. A string "TOF Left: " é seguida pelo valor da distância formatado com 2 casas decimais.
+    * Se o valor não couber na linha, ele é centralizado na tela, dividindo em duas linhas.
+    * 
+    * @param distance O valor da distância a ser exibido no LCD.
+    */
+    void postTofLeft(float distance);
+
+   /**
+    * @brief Exibe o valor da aceleração no LCD.
+    * 
+    * Este método exibe o valor da aceleração no LCD. A string "Acceleration: " é seguida pelo valor da aceleração formatado com 2 casas decimais.
+    * Se o valor não couber na linha, ele é centralizado na tela, dividindo em duas linhas.
+    * 
+    * @param acceleration O valor da aceleração a ser exibido no LCD.
+    */
+    void postAccelerometer(float acceleration);
+
+   /**
+    * @brief Exibe o ponto de ajuste (setpoint) no LCD.
+    * 
+    * Este método exibe o valor do ponto de ajuste no LCD. A string "SetPoint: " é seguida pelo valor do ponto de ajuste formatado com 2 casas decimais.
+    * Se o valor não couber na linha, ele é centralizado na tela, dividindo em duas linhas.
+    * 
+    * @param angle O valor do ponto de ajuste a ser exibido no LCD.
+    */
+    void postSetPoint(float angle);
+
+   /**
+    * @brief Exibe o modo operacional no LCD.
+    * 
+    * Este método exibe o modo operacional no LCD, centralizando a mensagem na tela. Pode ser um dos três modos: Manual, Automático, ou Ensaio.
+    * 
+    * @param modo O modo a ser exibido no LCD (Manual, Automático ou Ensaio).
+    */
+    void postOperatingMode(Modo modo);
+
+   /**
+    * @brief Exibe a medição de consumo de energia no LCD.
+    * 
+    * Este método exibe a medição de consumo de energia no LCD. A string "Consumption:" é seguida pela voltagem e corrente, com o respectivo símbolo de unidade.
+    * Se a mensagem não couber, o número de casas decimais é reduzido para 2.
+    * 
+    * @param voltage A voltagem medida a ser exibida no LCD.
+    * @param current A corrente medida a ser exibida no LCD.
+    * @param voltageUnit A unidade de voltagem a ser exibida.
+    * @param currentUnit A unidade de corrente a ser exibida.
+    */
+    void postConsumption(float voltage, float current, const char* voltageUnit, const char* currentUnit);
+
+   /**
+    * @brief Desliga o retroiluminamento do LCD.
+    * 
+    * Este método desliga o retroiluminamento do LCD.
+    */
+    void dashboardLightOn();
+
+   /**
+    * @brief Liga o retroiluminamento do LCD.
+    * 
+    * Este método liga o retroiluminamento do LCD.
+    */
+    void dashboardLightOff();
+
+   /**
+    * @brief Limpa a tela do LCD.
+    * 
+    * Este método limpa a tela do LCD e reposiciona o cursor no início.
+    */
     void clear();
+
+   /**
+    * @brief Limpa uma linha específica do LCD.
+    * 
+    * Este método limpa uma linha específica do LCD, e muda o cursor para a linha line e coluna 0.
+    * 
+    * @param line A linha que deve ser limpa. O valor deve estar entre 0 e o número máximo de linhas do LCD.
+    */
+    void clearLine(uint8_t line);
 
 };
 
